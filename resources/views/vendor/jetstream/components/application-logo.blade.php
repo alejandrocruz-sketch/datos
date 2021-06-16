f1cf621c0135905eedc5480c2959053182728979
Alejandro Cruz <alejandro.cruz@barracuda.com.co>
2021-06-16 09:02:57 -0500

    Edicion de los tags de Jets

---
+++ resources/views/vendor/jetstream/components/application-logo.blade.php
@@ -0,0 +1,3 @@
+<div class="flex justify-center pt-8 sm:pt-0">
+    <img class="h-56 w-auto text-gray-700" src="https://i.imgur.com/bhFu4Gn.png" alt="">
+</div>
---
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -0,0 +1,5 @@
+<a href="/">
+    <div class="flex justify-center pt-8 sm:pt-0">
+        <img class="h-40 w-auto text-gray-700" src="https://i.imgur.com/bhFu4Gn.png" alt="">
+    </div>
+</a>
